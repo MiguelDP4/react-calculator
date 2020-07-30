fd620ae289726186b3511bb6c7cad156a90d4e5b
MiguelDP4 <migueldp4@gmail.com>
2020-07-30 12:09:06 -0600

    Change code to comply with TSE's suggestions, even though the calculate method is not being used

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { calculate } from '../logic/calculate';
 
 export function Display(props) {
-  let { numberDisplay } = props;
-  // The next line is to use the calculate function and
-  // to remove the linter error, because the calculate
-  // function is not being used yet
-  numberDisplay = String(calculate({ next: '1', total: '1', operation: '-' }, '=').total);
+  const { numberDisplay } = props;
   return (
     <div id="Display">
       <span className="display-content" />
